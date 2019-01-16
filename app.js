157c700b8c1dcdf36a751dfbf125be0283b68028
Greg Kittleson <greg.kittleson@gmail.com>
2019-01-16 12:38:36 -0800

    Implement dynamically rendered GroceryList w/ line-out state

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-// TODO
+var groceryItems = ['milk', 'eggs', 'tater tots', 'frozen pizza'];
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gotIt: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      gotIt: !this.state.gotIt
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.gotIt ? 'line-through' : 'none'
+    };
+    var hover = {
+      'font-weight': 'bolder'
+    };
+
+    return (
+      <li
+        style={style}
+        onClick={this.onListItemClick.bind(this)}
+        onmouseover={hover}
+      >
+        {this.props.food}
+      </li>
+    );
+  }
+}
+
+var GroceryList = props => (
+  <ul>
+    {props.foods.map(food => (
+      <GroceryListItem food={food} />
+    ))}
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>Grocery List </h2>
+    <GroceryList foods={groceryItems} />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById('app'));
